b9d42a39fa8a5a28811638ea87c17aa5c6966c09
Tim <trc22@njit.edu>
2021-10-05 13:33:39 -0400

    Documentation

--- include/inventory.h
+++ include/inventory.h
@@ -22,22 +22,57 @@ typedef struct Item_S
     void *data;
 }Item;
 
+/**
+ * @brief initializes the inventory
+ * @param item_max the maximum amount of items the inventory can hold
+ */
 void inventory_init(Uint32 item_max);
 
 void inventory_update();
 
+/**
+ * @brief creates a new item and adds it the inventory
+ * @param item_max the maximum amount of items the inventory can hold
+ * @return item which was created, null on failure.
+ */
 Item* inventory_item_new();
 
+/**
+ * @brief removes an item from the inventory
+ * @param item to remove
+ */
 void inventory_remove_item(Item *item);
 
+/**
+ * @brief frees all items in the inventory
+ */
 void inventory_free();
 
+/**
+ * @brief frees an item in the inventory
+ * @param item to free
+ */
 void inventory_free_item(Item *item);
 
+/**
+ * @brief frees all items in the inventory and then frees the inventory itself
+ */
 void inventory_close();
 
+/**
+ * @brief creates an item with specific paramters and adds it to inventory
+ * @param id the unique item identification number
+ * @param name the item's name
+ * @param type one of the item types specified in ItemType enumeration
+ * @return item that was created
+ */
 Item* inventory_item_create(int id, char* name, ItemType type);
 
+/**
+ * @brief loads an item from items/items.json and adds it to the inventory
+ * @param item_name the key for the item in the json file
+ * @return the item that was created
+ */
 Item* inventory_load_item(char* item_name);
 
 
